e491547c3f5120922dbf0eca1fc8a3f012cdceb7
Evelyn Z <evelynzdragon@gmail.com>
2021-11-03 15:04:02 -0700

    setting up the repo forked from CodeSandbox; changed tsx to js file format

--- src/App.js
+++ src/App.js
@@ -4,10 +4,6 @@ export default function App() {
     return (
         <div className="App">
             <h1>Hello World!</h1>
-            <p>
-                Check the <span style={{ color: "orange" }}>instructions.md</span> to begin
-            </p>
-            <p>Good luck!</p>
         </div>
     );
 }
