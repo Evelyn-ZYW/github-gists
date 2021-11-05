32a03565f4b986109e43e906281b40c3b55bad77
Evelyn Z <evelynzdragon@gmail.com>
2021-11-04 18:01:13 -0700

    implemented infinite scroll; however found a bug - it keeps fetching when refreshing the page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,42 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useRef, useCallback } from "react";
 import useInfiniteScroll from "./useInfiniteScroll";
 import "./styles.scss";
 
 export default function App() {
-  //storing the returned data into items state
   const [pageNumber, setPageNumber] = useState(1);
-  //   const [isFetching, setIsFetching] = useInfiniteScroll(moreGists);
 
-  const { isFetching, error, items, hasMore } = useInfiniteScroll(pageNumber);
+  const { isFetching, error, items } = useInfiniteScroll(pageNumber);
+
+  const observer = useRef();
+  const lastItemRef = useCallback(
+    (node) => {
+      if (isFetching) return;
+      if (observer.current) observer.current.disconnect();
+      observer.current = new IntersectionObserver((entries) => {
+        if (entries[0].isIntersecting) {
+          console.log(items.length);
+          if (pageNumber < 2 && items.length < 60) {
+            setPageNumber((pageNum) => pageNum + 1);
+          } else {
+            // console.log(items.length);
+          }
+        }
+      });
+      if (node) observer.current.observe(node);
+    },
+    [isFetching, pageNumber]
+  );
 
   return (
     <div className="App">
-      {items.map((item) => (
-        <div className="line" key={item.id}>
+      {items.map((item, index) => (
+        <div
+          className="line"
+          key={item.node_id}
+          ref={items.length === index + 1 ? lastItemRef : null}
+        >
           <img src={item.owner.avatar_url} alt="" />
-          <p>{Object.keys(item.files)}</p>
+          <p>{Object.keys(item.files)[0]}</p>
         </div>
       ))}
       {isFetching && <div>Loading...</div>}
--- src/useInfiniteScroll.js
+++ src/useInfiniteScroll.js
@@ -4,32 +4,31 @@ export default function useInfiniteScroll(pageNumber) {
   const [isFetching, setIsFetching] = useState(true);
   const [error, setError] = useState(false);
   const [items, setItems] = useState([]);
-  const [hasMore, setHasMore] = useState(false);
 
   useEffect(() => {
     setIsFetching(true);
     setError(false);
 
-    requestGists().catch((err) => {
-      setIsFetching(false);
-      setError(`🧐 Please check the api. ${err.message}.`);
+    requestGists()
+        .catch((err) => {
+            setIsFetching(false);
+            setError(`🧐 Some error just happened. ${err.message}.`);
     });
-  }, []);
+  }, [pageNumber]);
 
   const requestGists = async () => {
-    const res = await fetch(`https://api.github.com/gists?page=${pageNumber}`);
+    const res = await fetch(
+      `https://api.github.com/gists?per_page=30&page=${pageNumber}`
+    );
 
-    //custom error message when there is a network error
     if (res.status !== 200) {
       throw new Error(setError("😵 It seems like a network error."));
     }
 
     const data = await res.json();
-    console.log(data);
-    setItems((previousItems) => [...previousItems, ...data]);
-    setHasMore(data.length > 0);
+    setItems((prevItems) => [...prevItems, ...data]);
     setIsFetching(false);
   };
 
-  return { isFetching, error, items, hasMore };
+  return { isFetching, error, items };
 }
